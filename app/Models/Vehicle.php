a522a795e763c044984cf4fd297bcdcf1fe73841
EddieFreedome <eddie.freedome@tutanota.com>
2023-09-01 14:28:59 +0200

    created controllers

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    // public function user()
+    // {
+    //     return $this->belongsTo(User::class, 'user_id');
+    // }
+
+    // public function rent()
+    // {
+    //     return $this->belongsTo(Rent::class, 'id');
+    // }
 }
